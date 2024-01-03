e6969f48dd00b1823773e44bd8b247bbc546c845
Sasha <sasha2232maximov@yandex.ru>
2024-01-03 16:47:55 +0300

    first update

---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.err.println("Hello, world!");
+        int a = 0;
+        int b = 5;
+        //умножение
+        System.out.println (a * b);
+        //деление
+        System.out.println (a / b);
+        //сумма
+        System.out.println (a + b);
+        //вычитание
+        System.out.println (a - b);
+    }
+}
