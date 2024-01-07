8056e529bc4898bd35fcd59db5571434d3f161a3
Sasha <sasha2232maximov@yandex.ru>
2024-01-07 13:21:54 +0300

    a

--- HardMath.java
+++ HardMath.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 
 public class HardMath {
     public static void main(String[] args) {
-        try (Scanner in = new Scanner(System.in))
-
-/*1.Возведение в степень*/
-                        int x; 
-
+        try (Scanner in = new Scanner(System.in)) {
+                double x = in.nextDouble();
+                double y = in.nextDouble();
 
 
+
+
+
+
+        }
     }
 }
