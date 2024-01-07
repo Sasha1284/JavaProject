b23a5d68b1e57bc41c20ed9cd4e082df296f9c04
Sasha <sasha2232maximov@yandex.ru>
2024-01-07 13:38:48 +0300

    update

--- HardMath.java
+++ HardMath.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
-
+import java.lang.Object;
 public class HardMath {
     public static void main(String[] args) {
         try (Scanner in = new Scanner(System.in)) {
                 double x = in.nextDouble();
                 double y = in.nextDouble();
-
-
+                int x1 = in.nextInt();
+                int y1 = in.nextInt();
+                System.out.println(Math.pow(x1,y1));
+                
 
 
 
